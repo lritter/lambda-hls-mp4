f650a933e9987238879b455229431313ed60bd18
Lincoln Ritter <lincoln@animoto.com>
2017-12-04 22:15:10 -0500

    Pull out operation from query string params. Respond with 302.

--- src/lambda.js
+++ src/lambda.js
@@ -4,8 +4,8 @@ const main = require('./main').main;
 
 exports.handler = (event, context, callback) => {
     const req = event;
-    const operation = req.operation;
-    delete req.operation;
+    const operation = req.operation || req.queryStringParameters.operation;
+
     console.log(req)
     if (operation) {
         console.log(`Operation ${operation} 'requested`);
@@ -16,9 +16,26 @@ exports.handler = (event, context, callback) => {
             callback(null, 'pong');
             break;
         case 'convert':
-            const hlsStream = 'https://s3.amazonaws.com/s3-p.animoto.com/Stream/wfuFvIlS99sjLoUkQTT2WA/hls.360p15.m3u8';
-            const renderTitle = 'bobs your uncle';
-            main(hlsStream, renderTitle, callback);
+            const hlsStream = req.queryStringParameters.input || 'https://s3.amazonaws.com/s3-p.animoto.com/Stream/wfuFvIlS99sjLoUkQTT2WA/hls.360p15.m3u8';
+            const renderTitle = req.queryStringParameters.title || 'bobs your uncle';
+            let makeResponse = (err, result) => {
+                let response = {};
+
+                if(!err) {
+                    response.statusCode = 302;
+                    response.headers = {
+                        "Location" : result.url.Location
+                    };
+                }
+                else {
+                    response.statusCode = 500;
+                    response.body = JSON.stringify({ error: err });
+                }
+
+                console.log("response: " + JSON.stringify(response))
+                callback(err, response);
+            };
+            main(hlsStream, renderTitle, makeResponse);
             break;
         default:
             callback(new Error(`Unrecognized operation "${operation}"`));
